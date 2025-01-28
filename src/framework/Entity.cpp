02069baca349f2941fa46294ed9c66b4df5b072f
AlejandroPoole <166758363+AlejandroPoole@users.noreply.github.com>
2025-01-28 14:49:14 +0100

    Add files via upload

---
+++ src/framework/Entity.cpp
@@ -0,0 +1,16 @@
+#include "Entity.h"
+
+void Entity::Render(Image* framebuffer, Camera* camera, const Color& c) {
+	const std::vector<Vector3> myVertices = mesh->GetVertices();
+	for (int i = 0; i < ? ? ? ;++i) { //For per iterar els triangles
+		Vector3 P0 = myVertices.at(i);
+		Vector3 P1= myVertices.at(i+1);
+		Vector3 P2= myVertices.at(i+2);
+
+		//Project Vector
+
+		//Clip to Framebuffer Space
+
+		//Draw Triangle
+	}
+}
---
+++ src/framework/Entity.h
@@ -0,0 +1,18 @@
+/*
+	The Mesh contains the info about how to render a mesh and also how to parse it from a file.
+*/
+
+#pragma once
+#include "mesh.h"
+#include "image.h"
+
+class Entity {
+public:
+	Mesh* mesh;
+	Matrix44 model;
+
+	Entity() {};
+	Entity(Matrix44 _model, Mesh* _mymesh) { mesh = _mymesh;model = _model; };
+
+	void Render(Image* framebuffer, Camera* camera, const Color& c);
+};
