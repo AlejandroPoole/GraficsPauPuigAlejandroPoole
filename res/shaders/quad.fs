38727a3d4025b7b416186e706a0502f6dbc1eeaa
PauPuigGuillen <pau.puig04@estudiant.upf.edu>
2025-02-18 18:23:42 +0100

    abc

--- res/shaders/quad.fs
+++ res/shaders/quad.fs
@@ -2,17 +2,26 @@ varying vec2 v_uv;
 
 void main()
 {
-	vec2 center = vec2(0.5,0.5);
-	vec2 v = center - v_uv; 
-	float leng = length(v);
-	vec3 finalColor = vec3(leng,leng,leng);
+	//b)
+	// vec2 center = vec2(0.5,0.5);
+	// vec2 v = center - v_uv; 
+	// float leng = length(v);
+	// vec3 finalColor = vec3(leng,leng,leng);
 	
-	gl_FragColor = vec4(finalColor,1.0);
+	// gl_FragColor = vec4(finalColor,1.0);
+	
 	//a)
 	// vec3 red_color = vec3(v_uv.x,0.0,0.0);
 	// vec3 blue_color = vec3(0.0,0.0,1.0-v_uv.x);
 
 	// vec3 finalColor = blue_color + red_color;
+	
+	// gl_FragColor = vec4(finalColor,1.0);
+
+	//c)
+	// vec3 red_color = vec3(step(sin(25*v_uv.x), 0.003), 0, 0);
+	// vec3 blue_color = vec3(0, 0, step(sin(25*v_uv.y), 0.003));
+	// vec3 finalColor = red_color + blue_color;
 	
 	// gl_FragColor = vec4(finalColor,1.0);
 }
