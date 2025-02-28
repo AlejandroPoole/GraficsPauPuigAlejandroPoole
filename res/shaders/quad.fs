cd060d771155af5e495e54b7fd23bc8045c5e5c8
PauPuigGuillen <pau.puig04@estudiant.upf.edu>
2025-02-28 14:10:26 +0100

    a, b, c, e

--- res/shaders/quad.fs
+++ res/shaders/quad.fs
@@ -62,13 +62,34 @@ void main()
 	}
 	else if(u_task == 2){
 		if (u_mode == 1.0){
+			//a)
 			vec4 texture_color = texture2D(u_texture, v_uv);
-			texture_color.x *= 0.5;
-			texture_color.y *= 0.5;
-			texture_color.z *= 0.5;
+			float average = (texture_color.x + texture_color.y + texture_color.z)/3;
+			texture_color.x = average;
+			texture_color.y = average;
+			texture_color.z = average;
 			gl_FragColor = texture_color;	
 		}
+		else if (u_mode == 2.0){
+			//b)
+			vec4 texture_color = texture2D(u_texture, v_uv);
+			float average = sin(texture_color.x + texture_color.y + texture_color.z)/3;
+			texture_color.x = 1-texture_color.x;
+			texture_color.y = 1-texture_color.y;
+			texture_color.z = 1-texture_color.z;
+			gl_FragColor = texture_color;
+		}
+		else if (u_mode == 3.0){
+			//c)
+			vec4 texture_color = texture2D(u_texture, v_uv);
+			float average = (texture_color.x + texture_color.y)/2;
+			texture_color.x = average;
+			texture_color.y = average;
+			texture_color.z = 0;
+			gl_FragColor = texture_color;
+		}
 		else if (u_mode == 5.0){
+			//e)
 			vec4 texture_color = texture2D(u_texture, v_uv);
 			vec2 center = vec2(0.5,0.5);
 			vec2 v = center - v_uv; 
